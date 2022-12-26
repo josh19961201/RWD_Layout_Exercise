cc0c5cb2b3e17eb57c027a57e4453636322cc4da
Josh Zhong <josh19961201@gmail.com>
2022-12-26 16:28:20 +0800

    準備製作testimonials區塊的輪播圖

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const banner = document.querySelector('#banner')
 const about = document.querySelector('#about')
+const testimonials = document.querySelector('#testimonials')
 
 window.addEventListener('scroll', function () {
   const scrollTop = window.pageYOffset
   const speed = 0.3
   banner.style.backgroundPosition = `0% ${scrollTop * speed}px`
   about.style.backgroundPosition = `0% ${scrollTop * speed}px`
+  testimonials.style.backgroundPosition = `0% ${scrollTop * speed}px`
 })
