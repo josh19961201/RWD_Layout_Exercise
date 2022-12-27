ba7b88ede5e9b5c1b54f935b2707e9ab67fb60fb
Josh Zhong <josh19961201@gmail.com>
2022-12-27 16:30:26 +0800

    Swiper設定中

--- js/main.js
+++ js/main.js
@@ -9,3 +9,31 @@ window.addEventListener('scroll', function () {
   about.style.backgroundPosition = `0% ${scrollTop * speed}px`
   testimonials.style.backgroundPosition = `0% ${scrollTop * speed}px`
 })
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+})
